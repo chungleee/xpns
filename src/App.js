e6bf8952bdcf332ee43147f73131e19d29f1f6bf
Leon <leon.karho.chung@gmail.com>
2019-10-18 13:06:07 -0400

    ListGrid and ExpenseList comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Balance from './components/Balance'
+import ListGrid from './components/ListGrid'
+import ExpenseList from './components/ExpenseList'
 
 const App = () => {
   return (
     <div>
       <Balance />
+      <ListGrid>
+        <ExpenseList />
+      </ListGrid>
     </div>
   )
 }
