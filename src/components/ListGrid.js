729844b5cc2f33ceaa60bd3b39b8a4e1a1e40a61
Leon <leon.karho.chung@gmail.com>
2019-10-18 13:08:57 -0400

    styling

--- src/components/ListGrid.js
+++ src/components/ListGrid.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const ListGrid = ({ children }) => {
-  return <div className="pl4 pr4">{children}</div>
+  return (
+    <div className="pl4 pr4" style={{ position: 'relative', top: '-45px' }}>
+      {children}
+    </div>
+  )
 }
 
 export default ListGrid
