928920a7d1369119fcfa6a68653f92be51997210
Leon <leon.karho.chung@gmail.com>
2019-10-18 13:08:47 -0400

    map data and return list with styling

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from 'react'
+
+const ExpenseList = () => {
+  const expenses = [
+    {
+      category: 'groceries',
+      cost: 1.25
+    },
+    {
+      category: 'food',
+      cost: 20
+    },
+    {
+      category: 'rent',
+      cost: 1300
+    },
+    {
+      category: 'groceries',
+      cost: 1.25
+    },
+    {
+      category: 'food',
+      cost: 20
+    },
+    {
+      category: 'rent',
+      cost: 1300
+    },
+    {
+      category: 'groceries',
+      cost: 1.25
+    },
+    {
+      category: 'food',
+      cost: 20
+    },
+    {
+      category: 'rent',
+      cost: 1300
+    }
+  ]
+
+  return (
+    <Fragment>
+      {expenses.map(({ category, cost }, idx) => {
+        return (
+          <li
+            key={idx}
+            className="bn shadow-4 br3 mt3 mb3 flex justify-between items-center h3 bg-white"
+          >
+            <p className="ml3 ttc">{category}</p>
+            <p className="mr3">${cost}</p>
+          </li>
+        )
+      })}
+    </Fragment>
+  )
+}
+
+export default ExpenseList
---
+++ src/components/ListGrid.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ListGrid = ({ children }) => {
+  return <div className="pl4 pr4">{children}</div>
+}
+
+export default ListGrid
