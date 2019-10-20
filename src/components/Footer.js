cb7bc5137f745436944d8e5a893f84cb34bdf51e
Leon <leon.karho.chung@gmail.com>
2019-10-20 17:03:58 -0400

    footer comp

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center bg-near-white h3 fixed bottom-0 w-100">
+      <div className="pl3 pr3">home</div>
+      <div className="pl3 pr3">add</div>
+      <div className="pl3 pr3">chart</div>
+    </footer>
+  )
+}
+
+export default Footer
