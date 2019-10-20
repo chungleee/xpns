5e6f6f92dbe76de1bdcd08adaa2c29aca7f2011f
Leon <leon.karho.chung@gmail.com>
2019-10-20 17:03:38 -0400

    add padding bottom so footer doesnt hide content

--- src/components/ListGrid.js
+++ src/components/ListGrid.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ListGrid = ({ children }) => {
   return (
-    <div className="pl4 pr4" style={{ position: 'relative', top: '-45px' }}>
+    <div className="pl4 pr4 pb4" style={{ position: 'relative', top: '-45px' }}>
       {children}
     </div>
   )
