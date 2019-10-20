7c9c238523c3c7c9c646553224644bf2bb3ecfad
Leon <leon.karho.chung@gmail.com>
2019-10-20 18:23:06 -0400

    basic virtual numpad placeholder

--- src/components/containers/AddExpense.js
+++ src/components/containers/AddExpense.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const AddExpense = () => {
-  return <div>this is the add expense comp</div>
+  return (
+    <Fragment>
+      <section>
+        <div>
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+        </div>
+        <div>
+          <span>4</span>
+          <span>5</span>
+          <span>6</span>
+        </div>
+        <div>
+          <span>7</span>
+          <span>8</span>
+          <span>9</span>
+        </div>
+        <div>
+          <span>0</span>
+        </div>
+      </section>
+    </Fragment>
+  )
 }
 
 export default AddExpense
