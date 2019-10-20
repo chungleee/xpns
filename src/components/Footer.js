ca56c80ab7298a3d821ffbe8705c074a82f8bd2c
Leon <leon.karho.chung@gmail.com>
2019-10-20 17:39:49 -0400

    styling - setup Link routing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-around items-center bg-near-white h3 fixed bottom-0 w-100">
-      <div className="pl3 pr3">
-        <i class="fas fa-home f3"></i>
-      </div>
-      <div className="pl3 pr3">
-        <i class="fas fa-plus f3"></i>
-      </div>
-      <div className="pl3 pr3">
-        <i class="fas fa-chart-pie f3"></i>
-      </div>
+    <footer
+      className="flex justify-around items-center bg-near-white h3 fixed bottom-0 w-100"
+      style={{ zIndex: '9999' }}
+    >
+      <Link to="/" className="link black dim pl3 pr3">
+        <i className="fas fa-home f3"></i>
+      </Link>
+      <Link to="/addexpense" className="link black dim pl3 pr3">
+        <i className="fas fa-plus f3"></i>
+      </Link>
+      <Link to="/chart" className="link black dim pl3 pr3">
+        <i className="fas fa-chart-pie f3"></i>
+      </Link>
     </footer>
   )
 }
