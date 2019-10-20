6a37c79f281b2fb589c9e5418cc87e711a3f46e9
Leon <leon.karho.chung@gmail.com>
2019-10-20 17:23:30 -0400

    fontawesome + icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center bg-near-white h3 fixed bottom-0 w-100">
-      <div className="pl3 pr3">home</div>
-      <div className="pl3 pr3">add</div>
-      <div className="pl3 pr3">chart</div>
+    <footer className="flex justify-around items-center bg-near-white h3 fixed bottom-0 w-100">
+      <div className="pl3 pr3">
+        <i class="fas fa-home f3"></i>
+      </div>
+      <div className="pl3 pr3">
+        <i class="fas fa-plus f3"></i>
+      </div>
+      <div className="pl3 pr3">
+        <i class="fas fa-chart-pie f3"></i>
+      </div>
     </footer>
   )
 }
