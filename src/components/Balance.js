e34bd6047a31c7b8da15900eee15c1f0373b13fd
Leon <leon.karho.chung@gmail.com>
2019-10-24 14:35:26 -0400

    misc

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ const Balance = () => {
   return (
     <div className="h5 flex justify-center items-center bg-red">
       <div className="tc washed-yellow">
-        <p className="f7 ttu">current balance</p>
+        <p className="f7 ttu">current expenses</p>
         <p className="f1 mt2 mb2">$500</p>
         <p className="f7">October 2019</p>
       </div>
