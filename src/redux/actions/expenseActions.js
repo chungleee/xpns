4907ef4646cbe6aea6d771d2ef1d618695463807
Leon <leon.karho.chung@gmail.com>
2019-10-28 11:46:43 -0400

    action creators: add and getAll

---
+++ src/redux/actions/expenseActions.js
@@ -0,0 +1,29 @@
+import db from '../../database'
+import moment from 'moment'
+import { ADD_EXPENSE, GET_ALL_EXPENSES } from './types'
+
+export const addExpense = expenseObj => {
+  return async dispatch => {
+    const payload = await db.put({
+      _id: moment()
+        .unix()
+        .toString(),
+      ...expenseObj
+    })
+
+    dispatch({
+      type: ADD_EXPENSE,
+      payload
+    })
+  }
+}
+
+export const getAllExpenses = () => {
+  return async dispatch => {
+    const payload = await db.allDocs({ include_docs: true })
+    dispatch({
+      type: GET_ALL_EXPENSES,
+      payload: payload.rows
+    })
+  }
+}
