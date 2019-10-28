6f9b81543818a4ab73452e5dbbb0f8aa079aa0ce
Leon <leon.karho.chung@gmail.com>
2019-10-28 12:44:50 -0400

    get state from store -> pass it as props

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getAllExpenses } from '../../redux/actions/expenseActions'
 
 import Balance from '../Balance'
@@ -8,8 +8,11 @@ import ExpenseList from '../ExpenseList'
 
 const Home = () => {
   const dispatch = useDispatch()
+  const expenses = useSelector(({ expenses }) => {
+    return expenses.allExpenses
+  })
 
-  // get date on mount
+  // get data on mount
   useEffect(() => {
     dispatch(getAllExpenses())
   }, [])
@@ -18,7 +21,7 @@ const Home = () => {
     <Fragment>
       <Balance />
       <ListGrid style={{ position: 'relative', top: '-45px' }}>
-        <ExpenseList />
+        <ExpenseList expenses={expenses} />
       </ListGrid>
     </Fragment>
   )
