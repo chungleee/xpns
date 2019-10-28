3d1280b04cfc32291128d09520810e6c1bfb31fd
Leon <leon.karho.chung@gmail.com>
2019-10-28 12:44:08 -0400

    remove dummy data - map prop

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,55 +1,21 @@
 import React, { Fragment } from 'react'
 
-const ExpenseList = () => {
-  const expenses = [
-    {
-      category: 'groceries',
-      cost: 1.25
-    },
-    {
-      category: 'food',
-      cost: 20
-    },
-    {
-      category: 'rent',
-      cost: 1300
-    },
-    {
-      category: 'groceries',
-      cost: 1.25
-    },
-    {
-      category: 'food',
-      cost: 20
-    },
-    {
-      category: 'rent',
-      cost: 1300
-    },
-    {
-      category: 'groceries',
-      cost: 1.25
-    },
-    {
-      category: 'food',
-      cost: 20
-    },
-    {
-      category: 'rent',
-      cost: 1300
-    }
-  ]
-
+const ExpenseList = ({ expenses }) => {
   return (
     <Fragment>
-      {expenses.map(({ category, cost }, idx) => {
+      {expenses.map(({ doc, id }) => {
         return (
           <li
-            key={idx}
-            className="bn shadow-3 br3 mt3 mb3 flex justify-between items-center h3 bg-white"
+            key={id}
+            className="bn shadow-3 br3 mt3 mb3 flex justify-between items-center h3 bg-white grow"
           >
-            <p className="ml3 ttc">{category}</p>
-            <p className="mr3">${cost}</p>
+            <div>
+              <p className="ml3 ttc">
+                {doc.category === 'other' ? doc.other : doc.category}
+              </p>
+              <small className="ml3">{doc.date}</small>
+            </div>
+            <p className="mr3">${doc.amount}</p>
           </li>
         )
       })}
