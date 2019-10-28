47c94dcf1d3950c837d00f2fc08929dfc420dda0
Leon <leon.karho.chung@gmail.com>
2019-10-28 11:44:03 -0400

    get all expenses from db on comp mount

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getAllExpenses } from '../../redux/actions/expenseActions'
+
 import Balance from '../Balance'
 import ListGrid from '../ListGrid'
 import ExpenseList from '../ExpenseList'
 
 const Home = () => {
+  const dispatch = useDispatch()
+
+  // get date on mount
+  useEffect(() => {
+    dispatch(getAllExpenses())
+  }, [])
+
   return (
     <Fragment>
       <Balance />
