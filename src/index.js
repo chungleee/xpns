14e5a4b54e22fe8c8c28431aab2eccd64b5bfed6
Leon <leon.karho.chung@gmail.com>
2019-10-28 11:44:40 -0400

    create store - applymiddlewares - redux devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import rootReducer from './redux/reducers'
 
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(rootReducer)
+// middlewares
+const middlewares = [thunk]
+// create store
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
