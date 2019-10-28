bc2780742e15484eb92a1130f0451468b4f7744a
Leon <leon.karho.chung@gmail.com>
2019-10-28 11:46:00 -0400

    small fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux'
 import expenseReducer from './expenseReducer'
 
 const rootReducer = combineReducers({
-  expense: expenseReducer
+  expenses: expenseReducer
 })
 
 export default rootReducer
