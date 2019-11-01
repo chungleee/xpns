706e385eab0a9431ef3f1ee01ca15b6cf1bf8d70
Leon <leon.karho.chung@gmail.com>
2019-11-01 15:14:34 -0400

    get total balance

--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -5,12 +5,15 @@ import { getAllExpenses } from '../../redux/actions/expenseActions'
 import Balance from '../Balance'
 import ListGrid from '../ListGrid'
 import ExpenseList from '../ExpenseList'
+import { getTotalBalance } from '../../utils'
 
 const Home = () => {
   const dispatch = useDispatch()
   const expenses = useSelector(({ expenses }) => {
     return expenses.allExpenses
   })
+
+  const balance = getTotalBalance(expenses)
 
   // get data on mount
   useEffect(() => {
@@ -19,7 +22,7 @@ const Home = () => {
 
   return (
     <Fragment>
-      <Balance />
+      <Balance balance={balance} />
       <ListGrid style={{ position: 'relative', top: '-45px' }}>
         <ExpenseList expenses={expenses} />
       </ListGrid>
